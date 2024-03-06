5354b09cfd2c52f673bdfb8552c295b0b3da9a28
MatiasDesimone <161649596+MatiasDesimone@users.noreply.github.com>
2024-03-06 20:12:51 -0300

    Add files via upload

---
+++ aaa/ultima clase/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pila.h"
+#include <time.h>
+#include <conio.h>
+
+
+int main()
+{
+//    1.- Sumar los elementos de una pila (usar variables enteras)
+
+
+//    Pila x, aux;
+//    inicpila(&aux);
+//    inicpila(&x);
+//    char control=0;
+//    int contador=0;
+//
+//
+//    printf("Cargando valores a la pila 'X'.\n\n");
+//
+//
+//    do
+//    {
+//        printf("Ingrese a la pila un valor entero:\n\n");
+//        fflush(stdin);
+//        leer(&x);
+//        printf("Para cargar mas elementos a la pila presione 's'.\n\n");
+//        fflush(stdin);
+//        scanf("%c", &control);
+//    }
+//    while(control == 's' || control == 'S');
+//
+//
+//    printf("Sumando los valores que posee la pila:\n\n");
+//    system("pause");
+//
+//
+//    while(!pilavacia(&x))
+//    {
+//        contador += tope(&x);
+//        apilar(&aux, desapilar(&x));
+//    }
+//
+//
+//    printf("El monto de los valores introducidos en la pila 'x' es %d", contador);
+//
+//
+//    while(!pilavacia(&aux))
+//    {
+//        apilar(&x, desapilar(&aux));
+//    }
+//
+//
+//    mostrar(&x);
+
+
+//    2.- Contar los elementos de una pila (usar variables enteras)
+
+//
+//    Pila x, aux;
+//    inicpila(&aux);
+//    inicpila(&x);
+//    char control=0;
+//    int contador=0;
+//
+//
+//    do
+//    {
+//        printf("Ingrese a la pila un valor entero:\n\n");
+//        fflush(stdin);
+//        leer(&x);
+//        printf("\n");
+//        printf("Presione cualquier tecla para cargar mas elementos a la pila, para finalizar presione 'Escape'.\n\n");
+//        fflush(stdin);
+//        control = getche();
+//    }
+//    while(control != 27);
+//
+//
+//    printf("Contando los valores que posee la pila:\n\n");
+//    system("pause");
+//
+//    while(!pilavacia(&x))
+//    {
+//        contador++;
+//        apilar(&aux, desapilar(&x));
+//    }
+//
+//
+//    printf("La cantidad de valores introducidos en la pila 'x' es %d", contador);
+//
+//
+//    while(!pilavacia(&aux))
+//    {
+//        apilar(&x, desapilar(&aux));
+//    }
+//
+//
+//    mostrar(&x);
+//
+
+
+
+
+    return 0;
+}
